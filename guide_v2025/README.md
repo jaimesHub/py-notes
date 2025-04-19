5110ed87b3421738e33d94257a0a90a70e670fc3
jaimesHub <duongquockhanh230596@gmail.com>
2025-04-19 16:48:23 +0700

    python basics - operators 1

--- guide_v2025/README.md
+++ guide_v2025/README.md
@@ -19,3 +19,4 @@
 - Shortcut (Format)
 - Integers
 - Floats
+- Operators
---
+++ guide_v2025/section_2/operators.py
@@ -0,0 +1,18 @@
+a: int = 2
+b: int = 4
+
+print(a // b) # floor division
+print(4 // 3) # floor division
+
+print(a ** b) # exponential operator - 2 asterisks
+
+print(a % b) # modulus operator gives remainder
+
+# order of operations
+print((10 + 10) * 2)
+
+x: int = 2
+x = x + 2
+# sugar syntax
+# x += 2
+print(x)
