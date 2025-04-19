b75a1b1920b1b4bdb0c73a9d5c43cbb268fea69f
jaimesHub <duongquockhanh230596@gmail.com>
2025-04-19 16:36:04 +0700

    python basics - integers

--- guide_v2025/README.md
+++ guide_v2025/README.md
@@ -16,3 +16,5 @@
 - Constants
 - Data types
 - Type Hints (Type annotation)
+- Shortcut (Format)
+- Integers
---
+++ guide_v2025/section_2/integers.py
@@ -0,0 +1,12 @@
+age: int = 30
+money: int = 100
+self_esteem: int = -50
+
+a: int = 5
+b: int = 10
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(type(a / b))
